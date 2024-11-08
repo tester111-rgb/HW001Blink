f326da276320e18fe82a581f8ca7db52b89d1d25
tester <nakamhi@gmail.com>
2024-11-08 19:56:56 +0700

    add serialprintln

--- src/main.cpp
+++ src/main.cpp
@@ -2,13 +2,16 @@
 
 void setup() {
   // initialize digital pin LED_BUILTIN as an output.
+  Serial.begin(9600);
   pinMode(2, OUTPUT);
 }
 
 // the loop function runs over and over again forever
 void loop() {
   digitalWrite(2, HIGH);  // turn the LED on (HIGH is the voltage level)
+  Serial.println("ON");
   delay(1000);                      // wait for a second
   digitalWrite(2, LOW);   // turn the LED off by making the voltage LOW
+  Serial.println("OFF");
   delay(1000);                      // wait for a second
 }
